ea860d29fbb88d2993ab1b6968b2dd577a377d82
Luca Wieczorek <info@lucawieczorek.de>
2020-05-26 16:59:41 +0200

    updated login-modal

--- src/FoodOrderSystem.App/ClientApp/src/app/login/login.component.ts
+++ src/FoodOrderSystem.App/ClientApp/src/app/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent implements OnInit {
           if (status === 401)
             this.message = "Benutzername und/oder Passwort ist nicht korrekt.";
           else
-            this.message = "Ein unvorhergesehener Fehler ist aufgetreten. Bitte versuchen Sie es erneut.";
+            this.message = "Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut.";
       });
   }
 }
