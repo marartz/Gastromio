d13ad5a33910c1148f9bb2375616326f12c45658
Christopher Peters <c_peters91@gmx.net>
2020-07-06 23:29:37 +0200

    Fixed some search and pagination related issues.
    Inter-component communication between server pagination component and it's parent has been changed, replaced on-change hooks by explicit calls (via @ViewChild) to make fetch/event flow more obvious. Also fixed reload of current page and added some fallbacks for empty page and invalid inputs like maxPages=0.
    
    Added search event to search input to track clear & enter button inputs.

--- src/FoodOrderSystem.App/ClientApp/src/app/admin-users/admin-users.component.ts
+++ src/FoodOrderSystem.App/ClientApp/src/app/admin-users/admin-users.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit, OnDestroy, ViewChild, AfterViewInit} from '@angular/core';
 import {UserAdminService} from '../user/user-admin.service';
 import {UserModel} from '../user/user.model';
 import {Subject} from 'rxjs';
@@ -8,15 +8,15 @@ import {AddUserComponent} from '../add-user/add-user.component';
 import {ChangeUserDetailsComponent} from '../change-user-details/change-user-details.component';
 import {ChangeUserPasswordComponent} from '../change-user-password/change-user-password.component';
 import {RemoveUserComponent} from '../remove-user/remove-user.component';
-import {ChangePageInfo} from '../pagination/server-pagination.component';
+import {FetchPageInfo, ServerPaginationComponent} from '../pagination/server-pagination.component';
 
 @Component({
   selector: 'app-admin-users',
   templateUrl: './admin-users.component.html',
   styleUrls: ['./admin-users.component.css', '../../assets/css/frontend.min.css', '../../assets/css/backend.min.css']
 })
-export class AdminUsersComponent implements OnInit, OnDestroy {
-  total: number;
+export class AdminUsersComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild(ServerPaginationComponent) pagingComponent: ServerPaginationComponent;
   pageOfUsers: UserModel[];
 
   private searchPhrase: string;
@@ -29,22 +29,41 @@ export class AdminUsersComponent implements OnInit, OnDestroy {
     this.searchPhraseUpdated.asObservable().pipe(debounceTime(200), distinctUntilChanged())
       .subscribe((value: string) => {
         this.searchPhrase = value;
-        this.updateSearch();
+        this.pagingComponent.triggerFetchPage(1);
       });
   }
 
   ngOnInit() {
     this.searchPhrase = '';
-    this.updateSearch();
+  }
+
+  ngAfterViewInit() {
+    // ViewChild has to be rendered before it can be accessed
+    this.pagingComponent.triggerFetchPage(1);
   }
 
   ngOnDestroy() {
+    this.searchPhraseUpdated?.unsubscribe();
+  }
+
+  onUpdateSearch(value: string) {
+    this.searchPhraseUpdated.next(value);
+  }
+
+  onFetchPage(info: FetchPageInfo) {
+    this.userAdminService.searchForUsersAsync(this.searchPhrase, info.skip, info.take)
+      .pipe(take(1))
+      .subscribe((result) => {
+        this.pageOfUsers = result.items;
+        this.pagingComponent.updatePaging(result.total, result.items.length);
+      }, () => {
+      });
   }
 
   openAddUserForm(): void {
     const modalRef = this.modalService.open(AddUserComponent);
     modalRef.result.then(() => {
-      this.updateSearch();
+      this.pagingComponent.triggerFetchPage();
     }, () => {
     });
   }
@@ -53,7 +72,7 @@ export class AdminUsersComponent implements OnInit, OnDestroy {
     const modalRef = this.modalService.open(ChangeUserDetailsComponent);
     modalRef.componentInstance.user = user;
     modalRef.result.then(() => {
-      this.updateSearch();
+      this.pagingComponent.triggerFetchPage();
     }, () => {
     });
   }
@@ -67,33 +86,9 @@ export class AdminUsersComponent implements OnInit, OnDestroy {
     const modalRef = this.modalService.open(RemoveUserComponent);
     modalRef.componentInstance.user = user;
     modalRef.result.then(() => {
-      this.updateSearch();
+      this.pagingComponent.triggerFetchPage();
     }, () => {
     });
-  }
-
-  onSearchType(value: string) {
-    this.searchPhraseUpdated.next(value);
-  }
-
-  onChangePage(changePageInfo: ChangePageInfo) {
-    this.userAdminService.searchForUsersAsync(this.searchPhrase, changePageInfo.skip, changePageInfo.take)
-      .pipe(take(1))
-      .subscribe((result) => {
-        this.total = result.total;
-        this.pageOfUsers = result.items;
-      }, () => {
-      });
-  }
-
-  updateSearch(): void {
-    this.userAdminService.searchForUsersAsync(this.searchPhrase, 0, 0)
-      .pipe(take(1))
-      .subscribe((result) => {
-        this.total = result.total;
-        this.pageOfUsers = result.items;
-      }, () => {
-      });
   }
 
   localizeRole(role: string): string {
