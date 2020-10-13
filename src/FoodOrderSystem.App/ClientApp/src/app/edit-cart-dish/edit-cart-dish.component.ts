b828941311bb0f45a92e7add604bafe35133e08e
Luca Wieczorek <info@lucawieczorek.de>
2020-10-13 14:49:23 +0200

    redesign: responsive & cross-browser design of edit-cart-dish-modal

--- src/FoodOrderSystem.App/ClientApp/src/app/edit-cart-dish/edit-cart-dish.component.ts
+++ src/FoodOrderSystem.App/ClientApp/src/app/edit-cart-dish/edit-cart-dish.component.ts
@@ -9,9 +9,7 @@ import {CartDishModel} from '../cart/cart-dish.model';
   templateUrl: './edit-cart-dish.component.html',
   styleUrls: [
     './edit-cart-dish.component.css',
-    '../../assets/css/frontend_v2.min.css',
-    '../../assets/css/backend_v2.min.css',
-    '../../assets/css/animations_v2.min.css'
+    '../../assets/css/frontend_v3.min.css'
   ]
 })
 export class EditCartDishComponent implements OnInit {
