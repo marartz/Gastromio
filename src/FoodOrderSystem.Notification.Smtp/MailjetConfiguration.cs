3cd5fa7571f35275fecee7f1e7c3d2165ea19e27
Marco Artz <artz.marco@gmx.net>
2020-11-10 23:17:17 +0100

    Changed notification adapter from Mailjet to classic SMTP.

---
+++ src/FoodOrderSystem.Notification.Smtp/MailjetConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodOrderSystem.Notification.Smtp
+{
+    public class SmtpConfiguration
+    {
+        public string ServerName { get; set; }
+        
+        public int Port  { get; set; }
+
+        public string UserName  { get; set; }
+        
+        public string Password { get; set; }
+    }
+}
