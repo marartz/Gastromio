ccf4a64d94f6097e659de4459ef9a9e9609c95f1
Marco Artz <artz.marco@gmx.net>
2020-11-28 19:50:02 +0100

    GH-97: Zahlungsmethoden im Checkout nicht alphabetisch sortiert

--- src/FoodOrderSystem.App/ClientApp/src/app/restaurant/restaurant.model.ts
+++ src/FoodOrderSystem.App/ClientApp/src/app/restaurant/restaurant.model.ts
@@ -44,6 +44,16 @@ export class RestaurantModel {
         this.externalMenus[i] = new ExternalMenu(this.externalMenus[i]);
       }
     }
+
+    if (this.paymentMethods) {
+      this.paymentMethods.sort((a, b) => {
+        if (a.name < b.name)
+          return -1
+        else if (a.name > b.name)
+          return 1;
+        return 0;
+      })
+    }
   }
 
 
