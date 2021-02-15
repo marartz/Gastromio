991d5e3eeb20c4226378c4fd7b0acba763f0d478
Marco Artz <artz.marco@gmx.net>
2021-02-15 15:42:12 +0100

    Added further tests.

---
+++ test/Gastromio.Domain.TestKit/Domain/Model/Cuisines/CuisineFactoryMock.cs
@@ -0,0 +1,16 @@
+using Gastromio.Core.Common;
+using Gastromio.Core.Domain.Model.Cuisines;
+using Gastromio.Core.Domain.Model.Users;
+using Moq;
+using Moq.Language.Flow;
+
+namespace Gastromio.Domain.TestKit.Domain.Model.Cuisines
+{
+    public class CuisineFactoryMock : Mock<ICuisineFactory>
+    {
+        public ISetup<ICuisineFactory, Result<Cuisine>> SetupCreate(string name, UserId createdBy)
+        {
+            return Setup(m => m.Create(name, createdBy));
+        }
+    }
+}
