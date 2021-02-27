88347e1a77c1770ab7eb4249b5a4a306e5fe7641
Luca Wieczorek <info@lucawieczorek.de>
2021-02-27 12:38:05 +0100

    fix: last commit

--- src/Gastromio.App/ClientApp/src/app/app.component.ts
+++ src/Gastromio.App/ClientApp/src/app/app.component.ts
@@ -6,7 +6,7 @@ import AOS from 'aos'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css','../../node_modules/aos/dist/aos.css'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
 
