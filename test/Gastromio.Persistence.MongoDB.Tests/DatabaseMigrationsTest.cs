6281b2b800c7e95b55f7164525c53a77a4f15373
ikke09 <development@fam-bechtluft.de>
2021-05-06 15:58:22 +0200

    Simple database integration tests

---
+++ test/Gastromio.Persistence.MongoDB.Tests/DatabaseMigrationsTest.cs
@@ -0,0 +1,26 @@
+﻿using Gastromio.Domain.TestKit.Common;
+using Xunit;
+using FluentAssertions;
+using Gastromio.Persistence.MongoDB.Migrations;
+
+namespace Gastromio.Persistence.MongoDB.Tests
+{
+    public class DatabaseMigrationsTest : DatabaseTestBase
+    {
+        [Fact]
+        [Trait("Category", TestCategoryConstants.IntegrationTest)]
+        public void AllValid_RunInitialSetup_ShouldBeSuccessful()
+        {
+            CurrentVersion.Should().Be(new MongoDBMigrations.Version(1, 0, 0));
+        }
+
+        [Fact]
+        [Trait("Category", TestCategoryConstants.IntegrationTest)]
+        public void AllValid_RunDatabaseMigrations_ShouldBeSuccessful()
+        {
+            var result = Migration.Run();
+            result.CurrentVersion.Should().Be(DatabaseVersions.LatestVersion);
+            IsDatabaseOutdated.Should().BeFalse();
+        }
+    }
+}
